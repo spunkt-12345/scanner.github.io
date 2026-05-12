f0a45855f180b94a9723f8c417f4f4d556600d0e
spunkt-12345 <f.horn@projob-rtk.de>
2026-05-12 11:39:47 +0200

    Add files via upload

---
+++ scanner.js
@@ -0,0 +1,13 @@
+document.addEventlistener("DOMContentLoaded", () => {
+
+    
+    const scanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    )
+    scanner.render(success)
+    
+    function success(decodeText, decodeResult) {
+        alert("!!!" + decodeText, decodeResult)
+    }
+})
